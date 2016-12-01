465a48703a3d866fb6c8bab0fe8e77d60e339ca8
Alex Vaidean <alexvaidean@gmail.com>
2016-12-01 13:33:47 +0200

    Add WriteString function

--- ExposureTimer.ino
+++ ExposureTimer.ino
@@ -29,8 +29,8 @@ int buttonCnt, buttonRead;
 /*******************************/
 
 /***** ENCODER STUFF HERE ******/
-static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
-static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
+//static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
+//static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
 volatile byte aFlag = 0; // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
 volatile byte bFlag = 0; // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
 volatile int encoderPos; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
@@ -51,19 +51,19 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(12, 2);
   // Print a message to the LCD.
-  lcd.setCursor(0, 0);
-  lcd.print("  Exposure  ");
-  lcd.setCursor(0, 1);
-  lcd.print("    Timer   ");
-  delay(2000);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("   Version  ");
-  lcd.setCursor(0, 1);
-  lcd.print("  v1.00b001 ");
-  delay(2000);
-  lcd.clear();
-  sprintf(timer1,"%02d:%02d:%02d", 0, encoderPos/60, encoderPos%60);
+//  lcd.setCursor(0, 0);
+//  lcd.print("  Exposure  ");
+//  lcd.setCursor(0, 1);
+//  lcd.print("    Timer   ");
+//  delay(2000);
+//  lcd.clear();
+//  lcd.setCursor(0, 0);
+//  lcd.print("   Version  ");
+//  lcd.setCursor(0, 1);
+//  lcd.print("  v1.00b001 ");
+//  delay(2000);
+//  lcd.clear();
+//  sprintf(timer1,"%02d:%02d:%02d", 0, encoderPos/60, encoderPos%60);
 }
 void clearLcd()
 {
@@ -105,64 +105,87 @@ void PinB(){
 void loop() {
 //  MyTimer.Timer(); // run the timer
 
-  if(oldEncPos != encoderPos)
-  {
-    oldEncPos = encoderPos;
-    clearLcd();
-  }
+  WriteString((char*)"Test1",0,0,0);
+  WriteString((char*)"Test2",7,0,1);
+  WriteString((char*)"Test3",0,1,2);
+//  if(oldEncPos != encoderPos)
+//  {
+//    oldEncPos = encoderPos;
+//    clearLcd();
+//  }
+//
+//  if (buttonPressed)
+//    menuLevel = menuLevelPrev;
+//  else
+//    menuLevel = encoderPos;    
+//
+//  lcd.setCursor(0, 0);
+//  switch (menuLevel)
+//  {
+//    case 0:
+//      menuLevelPrev = 0;
+//      lcd.print("Set Timer   ");
+//      if (buttonPressed)
+//      {
+//        sprintf(timer1,"%02d:%02d:%02d", 0, encoderPos/60, encoderPos%60);
+//        MyTimer.SetTimer(0, encoderPos/60, encoderPos%60);
+//      }
+//      break;
+//    case 1:
+//      menuLevelPrev = 1;
+//      if (buttonPressed)
+//      {
+//        lcd.print("Stop        ");
+//        MyTimer.Timer(); // run the timer
+//        timerHasStarted = 1;
+//        if (once)
+//        {
+//          MyTimer.StartTimer();
+//          once = 0;
+//        }
+//      }
+//      else
+//      {             
+//        lcd.print("Start       ");
+//        MyTimer.StopTimer();
+//        MyTimer.ResetTimer();
+//        timerHasStarted = 0;
+//      }
+//
+//      // print the number of seconds since reset:
+//      if (MyTimer.TimeHasChanged())
+//      {
+//        sprintf(timer1,"%02lu:%02lu:%02lu", MyTimer.ShowHours(), MyTimer.ShowMinutes(), MyTimer.ShowSeconds());
+//        clearLcd();
+//      }
+//      
+//      break;
+//    default:
+//      encoderPos = 0;
+//      break;
+//  }
+//
+//  KeyManager();
 
-  if (buttonPressed)
-    menuLevel = menuLevelPrev;
-  else
-    menuLevel = encoderPos;    
+  // do something when the timer expires
+//  if (MyTimer.TimeCheck() && timerHasStarted)
+//  {
+//    clearLcd();
+//    lcd.setCursor(0, 0);
+//    lcd.print("  DONE  "); 
+//    lcd.setCursor(0, 1);
+//    lcd.print("PRESS A KEY"); 
+//  }
+//  else
+//  {
+//    lcd.setCursor(0, 1);
+//    lcd.print(timer1);
+//  }
+}
 
-  lcd.setCursor(0, 0);
-  switch (menuLevel)
-  {
-    case 0:
-      menuLevelPrev = 0;
-      lcd.print("Set Timer   ");
-      if (buttonPressed)
-      {
-        sprintf(timer1,"%02d:%02d:%02d", 0, encoderPos/60, encoderPos%60);
-        MyTimer.SetTimer(0, encoderPos/60, encoderPos%60);
-      }
-      break;
-    case 1:
-      menuLevelPrev = 1;
-      if (buttonPressed)
-      {
-        lcd.print("Stop        ");
-        MyTimer.Timer(); // run the timer
-        timerHasStarted = 1;
-        if (once)
-        {
-          MyTimer.StartTimer();
-          once = 0;
-        }
-      }
-      else
-      {             
-        lcd.print("Start       ");
-        MyTimer.StopTimer();
-        MyTimer.ResetTimer();
-        timerHasStarted = 0;
-      }
-
-      // print the number of seconds since reset:
-      if (MyTimer.TimeHasChanged())
-      {
-        sprintf(timer1,"%02lu:%02lu:%02lu", MyTimer.ShowHours(), MyTimer.ShowMinutes(), MyTimer.ShowSeconds());
-        clearLcd();
-      }
-      
-      break;
-    default:
-      encoderPos = 0;
-      break;
-  }
-
-/***** DEBOUNCE STUFF HERE ******/
+void KeyManager(void)
+{
+  /***** DEBOUNCE STUFF HERE ******/
   buttonRead = digitalRead(4);
 
   // check to see if you just pressed the button
@@ -200,19 +223,53 @@ void loop() {
   // it'll be the lastButtonState:
   lastButtonState = buttonRead; 
 /********************************/
+}
 
-  // do something when the timer expires
-  if (MyTimer.TimeCheck() && timerHasStarted)
+/*
+ * Function Name:  WriteString 
+ * Description: Write a message at a specified possition on the LCD
+ * Paramters: 
+ *      msg (in) - message to be displayed in the LCD
+ *      x_pos (in) - x posistion on the LCD
+ *      y_pos (in) - y posistion on the LCD
+ *      option (in) - sets the mode in whitch the message is shown on the LCD
+ *                  - starting from the LSB: b0,b1 represent the status bits (ERASE, NORMAL, BLINK),
+ *                  the next 12 bits are for blinking the characters individualy
+ * Return: void
+ */
+void WriteString(char * msg, char x_pos, char y_pos, int option)
+{
+char blank[] = "            "; // string used to clear the line
+
+  // check if there is a message to be displayed and
+  // that the positions aren't over the limits
+  if ((msg == NULL) || (x_pos > 11) || (y_pos > 1))
+    return;
+
+  switch (option & 0x02)
   {
-    clearLcd();
-    lcd.setCursor(0, 0);
-    lcd.print("  DONE  "); 
-    lcd.setCursor(0, 1);
-    lcd.print("PRESS A KEY"); 
-  }
-  else
-  {
-    lcd.setCursor(0, 1);
-    lcd.print(timer1);
+    case 0: // erase LCD line
+      lcd.setCursor(x_pos, y_pos);
+      lcd.print(blank);
+      break;
+    case 1: // write the message on the LCD line
+      lcd.setCursor(x_pos, y_pos);
+      lcd.print(msg);
+      break;
+    case 2: // blink the message on the LCD line
+      lcd.setCursor(x_pos, y_pos);
+      lcd.print(msg);
+      delay(250);
+      // blink only some of the characters (in this case the 2nd and 3rd ones)
+      for (int i = 1; i < 3; i++)
+      {
+        lcd.setCursor(x_pos+i, y_pos);
+        lcd.print(blank[i]);
+      }
+      delay(250);
+      break;
+    default:
+      break;
   }
 }
+
